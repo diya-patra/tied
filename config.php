fd108636b44d2b0b1cb5f2640ac6fbe0c0ac219d
Diya Patra <diyapatra2nd@gmail.com>
2025-11-09 16:20:56 +0530

    Did all the cofig

--- config.php
+++ config.php
@@ -1,11 +1,14 @@
 <?php
+$env = parse_ini_file(__DIR__ . '/.env');
 // Tied Forum DB Connect
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'tied');
 
-$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$DB_SERVER = $env['DB_SERVER'];
+$DB_USERNAME = $env['DB_USER'];
+$DB_PASSWORD = $env['DB_PASS'];
+$DB_NAME = $env['DB_NAME'];
+
+$mysqli = new mysqli($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_NAME);
+
 if ($mysqli->connect_error) {
     die("ERROR: Could not connect. " . $mysqli->connect_error);
 }
